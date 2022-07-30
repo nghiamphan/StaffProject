3da8c4753929cedfecdb485079105384d3d0f6e3
Nghia Phan <nphan@fandm.edu>
2022-07-31 00:50:32 +0700

    need activeprofiles("test")

--- src/test/java/com/example/staff_project/service/StaffServiceTest.java
+++ src/test/java/com/example/staff_project/service/StaffServiceTest.java
@@ -12,11 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class StaffServiceTest {
     @Mock
     StaffRepository staffRepository;
