6ed4c7d775dd977d247c0c48bd1f2cd4a6c11d68
Nghia <pmnghia@IfiSolution.local>
2022-08-03 09:57:50 +0700

    test

--- src/test/java/com/example/staff_project/StaffEntityProjectApplicationTests.java
+++ src/test/java/com/example/staff_project/StaffEntityProjectApplicationTests.java
@@ -1,15 +1,20 @@
 package com.example.staff_project;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class StaffEntityProjectApplicationTests {
 
+	@Autowired
+	Environment environment;
 	@Test
 	void contextLoads() {
+		System.out.println("Get a property written by Jenkinsfile: " + environment.getProperty("prop1"));
 	}
 
 }
